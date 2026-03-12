63ec0431d47ddc626a3f04c24a399b3a800e910e
AnantSHA9 <248035324+AnantSHA9@users.noreply.github.com>
2026-03-13 00:41:59 +0530

    step 2: random interior walls added

--- world.py
+++ world.py
@@ -1,10 +1,12 @@
 # createing a 20*20 grid which will be the world map for the robot;
 import numpy as np
+import random as r
 class World:
     def __init__(self,size=20):
         self.size=size
         self.grid = np.zeros((size,size))
         self._set_border()
+        self._set_walls()
 
     def _set_border(self):
         self.grid[0,:] = 1
@@ -15,6 +17,22 @@ class World:
     def render(self):
         for rows in self.grid:
             print(''.join(["#" if cell==1 else "." for cell in rows]))
+ 
+    def _set_walls(self):
+       coor = []
+       for i in range(1,self.size-1):
+           for j in range(1,self.size-1):
+               coor.append((i,j))
+        
+       interior = self.size-2
+       k = int((interior*interior)*0.2)
+
+       r.shuffle(coor)
+
+       for i in range(k):
+            temp = coor[i]
+            self.grid[temp[0],temp[1]]=1
+
 
 
 
