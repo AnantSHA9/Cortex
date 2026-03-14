2851dcbe9748f7acd5c424832880ca951d470d30
AnantSHA9 <248035324+AnantSHA9@users.noreply.github.com>
2026-03-14 17:05:10 +0530

    clean reimplementation: steps 1-4

--- world.py
+++ world.py
@@ -1,75 +1,84 @@
-# createing a 20*20 grid which will be the world map for the robot;
+# complete reimplementation of the step 1-4;
+# goal :- 
+        ## to implement a world class which will create a entire 2d world map
+        ## to implent a 2d 20*20 grid 
+        ## all boundary will be considered as walls represented by 1 and free cell by 0
+        ## now to see grid we can either do print(world.grid) but that won't give us a good look 
+        ## so we need to implement a render function that will act a monitior which shows grid after every change
+        ## now as we have an inter map of the grid with wall on the boundary we need walls on 20% of the free walls inside boundary 
+        ## so we need to implement a place walls function for that
+        ## now we have walls and everything in place we need objects inside the world map
+        ## so objects can't be directly placed in side the grid because it won't work for 100 or 1000 objects 
+        ## so we will make a dictionary which will hold and object_name->type , coordinates, and we also need to mark those coordinates as filled as well
+
 import numpy as np
 import random as r
-class World:
+
+class World: 
     def __init__(self,size=20):
-        self.size=size
-        self.grid = np.zeros((size,size))
-        self.coor = []
-        self.k = 0 
-        self.object_dict = {}
-        self._set_border()
+        self.size = size
+        self.grid = np.zeros((size,size),dtype=int)
+        self.k = 0
+        self.coor=[]
+        self.free_cells=[]
+        self.obj_dict={}
+
         self._set_walls()
+        self._place_walls()
+
         self.free_cells = self.coor[self.k:]
 
-# creating a set border function which runs the moment an object is created and initilize the borders with '1';
-    def _set_border(self):
-        self.grid[0,:] = 1
-        self.grid[-1,:] = 1
-        self.grid[:,0] = 1
-        self.grid[:,-1] = 1
 
-# created a render function which shows how the grid map looks 
-    def render(self): 
-     for i in range(self.size):
-         row_string = ""
-         for j in range(self.size):
-              flag = 0
-              if self.grid[i][j] == 1:
-                   row_string+="#"
-              elif self.grid[i][j] == 0:
-                 for obj_name,obj_data in self.object_dict.items():
-                        if obj_data["position"]==(i,j):
-                            flag = 1
-                            row_string+=obj_data["type"][0].upper()
-                            break
-                 if not flag:
-                        row_string+="."
-
-         print(row_string)
-                                
-# creating a set walls function it's a private function which places walls inside the grid;
-# from position (1,1)->(18,18) and each time walls are placed at random positions because of r.suffle();
     def _set_walls(self):
-       for i in range(1,self.size-1):
-           for j in range(1,self.size-1):
-               self.coor.append((i,j))
+        self.grid[0,:]=1
+        self.grid[-1,:]=1
+        self.grid[:,0]=1
+        self.grid[:,-1]=1
+
+    def render(self):
+        for i in range(self.size):
+            row_build = ""
+            for j in range(self.size):
+                flag = 0
+                if self.grid[i][j]==1:
+                    row_build+="#"
+                elif self.grid[i][j]==0:
+                    for obj_name,obj_data in self.obj_dict.items():
+                        if obj_data["location"]==(i,j):
+                            flag=1
+                            row_build+=obj_data["type"][0].upper()
+                            break
+                    if not flag:
+                        row_build+="."
+            print(row_build)
+
+    def _place_walls(self):
+        for i in range(1,self.size-1):
+            for j in range(1,self.size-1):
+                self.coor.append((i,j))
+
+        int_size = (self.size-2)**2
+        self.k  = int((int_size*0.2))
+
+        r.shuffle(self.coor)
         
-       interior = self.size-2
-       self.k = int((interior*interior)*0.2)
+        for i in range(self.k):
+            x,y = self.coor[i]
+            self.grid[x][y]=1
 
-       r.shuffle(self.coor)
 
-       for i in range(self.k):
-            x,y= self.coor[i]
-            self.grid[x,y]=1
-
-# creating a place objects functions which creates a dictionary to maintain which location has what object place;
-# and we can see that by render function using the first initials of the object's name;
-    def place_objects(self,obj_type,obj_name):
-        if len(self.free_cells)>=1:
-          indxs = self.free_cells.pop(0)
-        else:
-          print("No free position")
-
-        self.object_dict[obj_name]={"type":obj_type,"position":indxs}
+    def place_objects(self,obj_name,obj_type): 
+        if len(self.free_cells)>0:
+          indx = self.free_cells.pop()
+        self.obj_dict[obj_name]={"type":obj_type,"location":indx}
 
 
 
 
-
-world = World()
-world.place_objects("chair","chair_1")
-world.place_objects("person","person_1")
-print(world.object_dict)
+                 
+world=World()
+world.place_objects("chair_1","chair")
+world.place_objects("chair_2","chair")
+world.place_objects("person_1","person")
+world.place_objects("table_1","table")
 world.render()
