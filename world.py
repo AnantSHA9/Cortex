9ab138f0871b52f88d569e647e82bc42e8d7ec9a
AnantSHA9 <248035324+AnantSHA9@users.noreply.github.com>
2026-03-18 13:09:07 +0530

    reimplementation: world with objects

--- world.py
+++ world.py
@@ -1,123 +1,67 @@
-# complete implementation of the world map
-# goal :- 
-        ## to implement a world class which will create a entire 2d world map
-        ## to implent a 2d 20*20 grid 
-        ## all boundary will be considered as walls represented by 1 and free cell by 0
-        ## now to see grid we can either do print(world.grid) but that won't give us a good look 
-        ## so we need to implement a render function that will act a monitior which shows grid after every change
-        ## now as we have an inter map of the grid with wall on the boundary we need walls on 20% of the free walls inside boundary 
-        ## so we need to implement a place walls function for that
-        ## now we have walls and everything in place we need objects inside the world map
-        ## so objects can't be directly placed in side the grid because it won't work for 100 or 1000 objects 
-        ## so we will make a dictionary which will hold and object_name->type , coordinates, and we also need to mark those coordinates as filled as well
+# goal: to implement a 20*20 2d grid which is our world view
+      ## -> the grid will have walls on it's boundary 
+      ## -> and inside 20% of indices will be walls which comes randomly not hard coded
+      ## -> after that objects needs to appear inside grid 
+      ## -> but the object can't be stored in the grid so we will need a container to store them with it's object tyep and it's location
+      ## -> then we will need a render function that's going to print the grid with walls free indices and objects as well
+      ## -> render function is for our use because it will provide information on how things are working printing the grind won't provide any information
 
 import numpy as np
 import random as r
 
-class World: 
-    def __init__(self,size=20):
-        self.size = size
-        self.grid = np.zeros((size,size),dtype=int)
+class World:
+    def __init__(self):
+        self.size = 20
+        self.grid = np.zeros((self.size,self.size),dtype=int)
+        self.coords = []
         self.k = 0
-        self.coor=[]
-        self.free_cells=[]
-        self.obj_dict={}
+        self._put_walls()
+        self.free_cells = self.coords[self.k:]
+        self.obj_dict = {}
+        
+        
 
-        self._set_walls()
-        self._place_walls()
-
-        self.free_cells = self.coor[self.k:]
-
-
-    def _set_walls(self):
+    def _put_walls(self):
         self.grid[0,:]=1
-        self.grid[-1,:]=1
+        self.grid[-1,:]=1 
+
         self.grid[:,0]=1
         self.grid[:,-1]=1
+        
+        self.coords = [(i,j) for i in range(1,self.size-1) for j in range(1,self.size-1)]
+        self.k = int(((self.size-2)*(self.size-2))*0.2)
+        r.shuffle(self.coords)
 
-    def render(self,robot):
-        self.robot = robot # remove this line after realizing the mistake
+        for i in range(self.k):
+            x,y = self.coords[i]
+            self.grid[x][y] = 1 
+
+    def put_objects(self,obj_type,obj_name):
+            x,y = self.free_cells.pop()
+            self.obj_dict[obj_name]={"type":obj_type,"pos":(x,y)}
+
+    def render(self):
         for i in range(self.size):
-            row_build = ""
+            row_build=""
             for j in range(self.size):
                 flag = 0
                 if self.grid[i][j]==1:
                     row_build+="#"
                 elif self.grid[i][j]==0:
-                    if (self.robot.i,self.robot.j)==(i,j):
-                        row_build+="R"
-                        flag = 1
-                        continue
-
                     for obj_name,obj_data in self.obj_dict.items():
-                        if obj_data["location"]==(i,j):
-                            flag=1
-                            row_build+=obj_data["type"][0].upper()
-                            break
+                         if (i,j) == obj_data["pos"]:
+                             row_build+=obj_data["type"][0].upper()
+                             flag = 1 
+                             break
                     if not flag:
-                        row_build+="."
-                    
-                        
+                      row_build+="."
             print(row_build)
+          
 
-    def _place_walls(self):
-        for i in range(1,self.size-1):
-            for j in range(1,self.size-1):
-                self.coor.append((i,j))
-
-        int_size = (self.size-2)**2
-        self.k  = int((int_size*0.2))
-
-        r.shuffle(self.coor)
-        
-        for i in range(self.k):
-            x,y = self.coor[i]
-            self.grid[x][y]=1
+world = World()
+world.put_objects("chair","chair_1")
+world.put_objects("table","table_1")
+world.put_objects("stairs","stairs_1")
+world.render()
 
 
-    def place_objects(self,obj_name,obj_type): 
-        if self.free_cells:
-          indx = self.free_cells.pop()
-          self.obj_dict[obj_name]={"type":obj_type,"location":indx}
-
-
-
-# complete implementation of robot class 
-# goal :- 
-       ## putting a robot inside the world map whcih we created earlier 
-       ## and giving it movements in the grid 
-
-class Robot:
-    def __init__(self,world):
-        self.world = world    
-        self.initial_pos = self.world.free_cells[0] # remove [0] and use .pop() after realizing the mistkae with [0]
-        self.i,self.j = self.initial_pos
-
-
-    def movement(self,direction):
-    ## make this entire check functionality to move to a different check function which return bool for better architecture
-       if direction=="up":
-           if self.world.grid[self.i-1][self.j]==0:
-               self.i = self.i-1
-       elif direction == "down":
-            if self.world.grid[self.i+1][self.j]==0:
-               self.i = self.i+1
-       elif direction == "right":
-            if self.world.grid[self.i][self.j+1]==0:
-               self.j = self.j+1
-       elif direction == "left":
-            if self.world.grid[self.i][self.j-1]==0:
-               self.j = self.j-1
-
-
-
-world=World()
-robot=Robot(world)
-world.place_objects("chair_1","chair")
-world.place_objects("chair_2","chair")
-world.place_objects("person_1","person")
-world.place_objects("table_1","table")
-robot.movement("up")
-robot.movement("down")
-robot.movement("left")
-world.render(robot)
